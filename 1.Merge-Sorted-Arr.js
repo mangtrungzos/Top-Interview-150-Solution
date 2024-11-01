e172250c507bd2aba8809030357e7b07d8c40385
mangtrungzos <sangv906@gmail.com>
2024-11-01 17:10:09 +0700

    1/11 Solution 1 in 150 Solution

--- 1.Merge-Sorted-Arr.js
+++ 1.Merge-Sorted-Arr.js
@@ -62,3 +62,36 @@ console.log(nums1);
 //              The result of the merge is [1].
 //              Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
 
+/**
+ * nums1 = [1, 2, 3, 0, 0, 0] | nums2 = [2, 5, 6] | m = 3 | n = 3
+ * Target : merge nums2 -> nums1
+ * Intialize pointer : p1, p2, p
+ * p1 = m - 1 : pointer to the last ele of nums1
+ * p2 = n - 1 : pointer to the last ele of nums2
+ * p = m + n - 1 : pointer to the last ele of nums1 (where the last ele after being merged will be)
+ *
+ * 1. while ( p1(2) >= 0 && p2(2) >= 0) true
+ *    => if (nums1[2] : 3 > nums2[2] : 6) false
+ *    => else (nums1[p] : 5 = nums2[p2] : 2 ) nums1[5] = 6 | p2-- : 1 | p-- : 4
+ *    => nums1 = [1, 2, 3, 0, 0, 6]
+ *
+ * 2. while (p1(2) >= 0 && p2(1) >= 0) true
+ *    => if (nums1[2] : 3 > nums2[1] : 5) false
+ *    => else (nums1[p] :4 = nums2[p2] : 1) nums1[4] = 5 | p2-- : 0 | p-- : 3
+ *    => nums1 = [1, 2, 3, 0, 5, 6]
+ *
+ * 3. while (p1(2) >=0 %% p2(0) >= 0) true
+ *    => if (nums1[2] : 3 > nums2[0] : 2) true
+ *    => nums1[3] = nums1[p1] : 3 | p1-- : 1 | p-- : 2
+ *    => nums1 = [1, 2, 3, 3, 5, 6]
+ *
+ * 4. while (p1(1) >= 0 && p2(0) >= 0) true
+ *    => if (nums1[1] : 2 > nums2[0] : 2) false
+ *    => else (nums1[p] : 2 = nums2[p2] : 0 = 2) nums1[2] = 2 | p2-- : -1 | p-- : 1
+ *    => nums1 = [1, 2, 2, 3, 5, 6]
+ *
+ * 5 while ( false ) exit loop
+ *   while (p2 >= 0) false | exit loop
+ *
+ * => final res: nums1 = [1, 2, 2, 3, 4, 5, 6]
+ */
---
+++ Context.md
@@ -0,0 +1,6 @@
+# Pointer
+- Là một biến đặc biệt lưu trữ địa chỉ bộ nhớ của biến khác
+- Thay vì lưu giá trị trực tiếp | Pointer lưu vị trí nơi giá trị đó được lưu trong bộ nhớ
+- Tiết kiệm bô nhớ: pointer làm việc với cấu trúc dữ liệu không cần sao chép
+- pointer can change data in functions without returning a value
+- 
